69c97d2fa28ee1e8e1504fd9f5d96104121b6ffb
Swastik Awaze <swastikawaze@gmail.com>
2024-05-20 20:32:29 +0530

    Add project files.

---
+++ CRUDAppUsingASPCoreWebAPI/Models/Student.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CRUDAppUsingASPCoreWebAPI.Models
+{
+    public class Student
+    {
+       
+        public int id { get; set; }
+        [Required]
+        public string name { get; set; }
+        [Required]
+        public string fname { get; set; }
+        [Required]
+        public string email { get; set; }
+        [Required]
+        public string mobile { get; set; }
+        [Required]
+        public string description { get; set; }
+       
+
+
+
+    }
+}
